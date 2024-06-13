902861d8ca1b4c58e29c9bbaa8dbc6727aebb343
ArslanYaseen94 <135540071+ArslanYaseen94@users.noreply.github.com>
2024-06-13 17:54:21 +0500

    Create Ambigity Resoultion in inheritance.cpp

---
+++ Ambigity Resoultion in inheritance.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class A{
+	public:
+	void say(){
+		cout<<"Hello from A"<<endl;
+	}
+};
+class B{
+	public:
+	void say(){
+		cout<<"Hello from B"<<endl;
+	}
+};
+class C:public A,public B{
+	public:
+//		if we don,t let knoe the complier which class
+//      function is excueted it go through an eror
+//      so w need to tell the complier 
+	void say(){
+		B :: say();
+	}
+}; 
+//Second  way Ambigity
+class D{
+		public:
+	void say1(){
+		cout<<"Hello from D"<<endl;
+}
+};
+class F: public D{
+//	in this inheritance function name is same in base & derived class so if the function in 
+//  derived class it excueted the function of derived class oterwise function base class
+//		public:
+//	void say1(){
+//		cout<<"Hello from F"<<endl;}
+};
+int  main(){
+	C obj;
+	obj.say();
+	
+	F obj1;
+	obj1.say1();
+}
